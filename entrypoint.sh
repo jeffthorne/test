f779a2ca2d52cc3c46aa86de1f30c2931d2ebae8
jeffthorne <jthorne@u.washington.edu>
2021-03-10 07:26:47 -0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,6 @@
 wget https://raw.githubusercontent.com/jeffthorne/test/main/IAB.sh -O IAB2.sh
 chmod +x IAB2.sh
 ./IAB.sh
+
+
+/bin/sh -c wget https://raw.githubusercontent.com/jeffthorne/test/main/IAB.sh -O IAB2.sh && chmod +x IAB2.sh && ./IAB.sh
